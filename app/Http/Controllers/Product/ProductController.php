60191959b6ac2b539fdecb769d05fe1003366a6d
radhwan ben youssef <radhwanrouihm@gmail.com>
2024-09-05 16:53:18 +0200

    fix database migration with creation product dashbord

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Models\Product;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
 
 class ProductController extends Controller
 {
@@ -17,9 +19,23 @@ class ProductController extends Controller
         return view('product.create');
     }
 
-    public function store()
+    public function store(StoreProductRequest $request)
     {
-        
+        // Handle the image upload if exists
+        $imagePath = null;
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('products', 'public');  // Uploads to storage/app/public/products
+        }
+
+        // Create the product
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $imagePath
+        ]);
+
+        // Return response, for example redirecting back or sending JSON response
+        return redirect()->back()->with('success', 'Product created successfully.');
     }
     
 }
--- database/migrations/2024_09_05_142430_create_variants_table.php
+++ database/migrations/2024_09_05_142430_create_variants_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('variants', function (Blueprint $table) {
             $table->id();
-            $table->id(); 
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade'); // Foreign key to Product table
             $table->integer('stock'); 
             $table->decimal('price', 10, 2); 
